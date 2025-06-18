1995af45c632f664da49e81fbece10bebb2b0f7d
TheNewRetr0 <oscarluna1318@gmail.com>
2025-06-18 00:15:50 -0600

    WAWAW

--- js/script.js
+++ js/script.js
@@ -62,3 +62,29 @@ showMessage();
       button.classList.add('clicked');
     });
   });
+
+function copyEmail() {
+  const email = "oscarluna1318@gmail.com";
+  navigator.clipboard.writeText(email).then(function() {
+    const msg = document.getElementById("copyMessage");
+    msg.style.display = "inline";
+    setTimeout(() => {
+      msg.style.display = "none";
+    }, 2000); // hide after 2 seconds
+  }, function(err) {
+    console.error("Failed to copy email: ", err);
+  });
+}
+
+function copyPhone() {
+  const phone = "+526565790106";
+  navigator.clipboard.writeText(phone).then(function() {
+    const msg = document.getElementById("phoneMessage");
+    msg.style.display = "inline";
+    setTimeout(() => {
+      msg.style.display = "none";
+    }, 2000);
+  }, function(err) {
+    console.error("Error al copiar el número: ", err);
+  });
+}
